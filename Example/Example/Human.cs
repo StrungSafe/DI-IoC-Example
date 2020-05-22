9b86262dfd07c96ed11c21737904fa5bca1f4eb7
Miguel Molina Jr <molim134@gmail.com>
2020-05-21 21:48:37 -0400

    Initial check-in for DI example

---
+++ Example/Example/Human.cs
@@ -0,0 +1,86 @@
+﻿namespace Example
+{
+    using System;
+
+    public class Human
+    {
+        public void EatFood()
+        {
+            var orange = new Orange();
+            orange.Peel();
+            orange.Eat();
+
+            var steak = new Steak();
+            steak.Cook();
+            steak.Eat();
+        }
+    }
+
+    public class Orange
+    {
+        private bool isPeeled;
+
+        private int percentOfOrangeLeft;
+
+        public Orange()
+        {
+            isPeeled = false;
+            percentOfOrangeLeft = 100;
+        }
+
+        public void Eat()
+        {
+            if (percentOfOrangeLeft == 0)
+            {
+                throw new NoOrangeLeftException();
+            }
+
+            percentOfOrangeLeft = 0;
+        }
+
+        public void Peel()
+        {
+            if (!isPeeled)
+            {
+                isPeeled = true;
+            }
+        }
+    }
+
+    public class Steak
+    {
+        private readonly int percentOfSteakLeft;
+
+        private bool isCooked;
+
+        public Steak()
+        {
+            isCooked = false;
+            percentOfSteakLeft = 100;
+        }
+
+        public void Cook()
+        {
+            if (!isCooked)
+            {
+                isCooked = true;
+            }
+        }
+
+        public void Eat()
+        {
+            if (percentOfSteakLeft == 0)
+            {
+                throw new NoSteakLeftException();
+            }
+        }
+    }
+
+    public class NoOrangeLeftException : Exception
+    {
+    }
+
+    public class NoSteakLeftException : Exception
+    {
+    }
+}
