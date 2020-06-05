6e9c794ed1bf31351a8a295f0fa90eb79eef201e
Miguel Molina Jr <molim134@gmail.com>
2020-06-04 20:26:56 -0400

    comments

--- Example/Example.DI.Console/Program.cs
+++ Example/Example.DI.Console/Program.cs
@@ -21,7 +21,7 @@
                 var human = new Human();
 
                 // We can further abstract this logic (and getting the input too) further to allow for flexibility
-                // DI containers will help to select the proper class
+                // DI frameworks will help to select the proper class
                 if (input == "orange")
                 {
                     food = new Orange();
@@ -40,6 +40,7 @@
                 }
 
                 // The human class is no longer controlling the food it should eat, it is being handed food that it then eats
+                // We can extend this idea to a potential stomach class if we need one
                 human.EatFood(food);
 
                 Console.WriteLine("Thanks, that was good");
